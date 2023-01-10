b6aa060bbcc9b142435ecea4826fd5d6b1536864
wengseng <wengseng@crenergy.com.my>
2023-01-10 23:43:04 +0800

    fix generate in statement for empty array

--- global.js
+++ global.js
@@ -3,6 +3,10 @@ const github_fun = () => { console.log('test'); }
 const generateInStatement = async (data) => {
   let inStatement = '';
   
+  if (data.length === 0) {
+    return await `('')`;
+  }
+  
   data.forEach((item) => {
       inStatement += `, '${item}' `;
   })
